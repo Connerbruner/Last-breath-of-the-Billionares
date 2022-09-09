32cff7c8cbe2842b149d5c61f477ee1348fec376
ConnerBrunner <Con-bru000@g.rentonschools.us>
2022-09-09 07:56:05 -0700

    AI

--- EmmiTraining.java
+++ EmmiTraining.java
@@ -1,14 +1,23 @@
 public class EmmiTraining extends FileRead {
+    boolean isTraining = true;
 
+    public void massTrain( int count ) {
+    }
+
+    public void train( Emmi emmi ) {
+
+        int healthScore = 0;
+        int damageScore = 0;
+        int total       = damageScore + healthScore;
+    }
 }
- /**
-  * Get you back in the loop with lbob
-  *
-  * Stuff we need to make:
-  *
-  * Create Ai file import most stuff from the old project
-  * Create a training ver of battle that takes in a emmi or boss
-  * Create a read write weights method FOR EACH EMMI AND BOSS WHOOOOOO
-  * Create battle score ratio
-  *
+/**
+ * Get you back in the loop with lbob
+ * <p>
+ * Stuff we need to make:
+ * <p>
+ * Create Ai file import most stuff from the old project
+ * Create a training ver of battle that takes in a emmi or boss
+ * Create a read write weights method FOR EACH EMMI AND BOSS WHOOOOOO
+ * Create battle score ratio
  **/
